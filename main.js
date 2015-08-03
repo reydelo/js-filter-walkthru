c98c1ca01a0029bf9b52f3457aa75414fcae8d03
reydelo <reyna.deloge@gmail.com>
2015-08-03 16:20:47 -0600

    add functionality for filter

--- main.js
+++ main.js
@@ -6,3 +6,13 @@
 2. Pass the value to a helper function to find values in arr that are in the range
 3. Append values back to DOM
 */
+
+$('button').on('click', function(){
+  var domValues = [];
+  $('#original-values li').each(function(i, li){
+    domValues.push($(li).text());
+  });
+  var upperLimit = $(this).data('val');
+  var range = getRanges(upperLimit);
+  getValues(range, domValues);
+})
--- utility.js
+++ utility.js
@@ -1,1 +1,23 @@
 // Utility functions
+function getRanges(num){
+  switch (num) {
+    case 10:
+      return [0, 10];
+    case 20:
+      return [11, 20];
+    case 30:
+      return [21, 30];
+    case 40:
+      return [31, 40];
+  }
+}
+
+function getValues(rangeArr, domArr) {
+  var newArr = [];
+  for(var i = 0; i < domArr.length; i++) {
+    if (domArr[i] >= rangeArr[0] && domArr[i] <= rangeArr[1]) {
+      newArr.push(domArr[i]);
+    }
+  }
+  return newArr;
+}
